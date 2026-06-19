12287fdc8264fbe4d81eed425dd119ad0f47b732
dharmayudho <sudarmayudhoprayitno@gmail.com>
2026-06-19 07:43:08 +0700

    penmabahan assets gambar dan juga sedikit setup di BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -1,68 +1,83 @@
-import { SCENES, COLORS, TILE } from "../config/GameConfig";
+import { SCENE, TILE } from "../config/GameConfig";
 
 export default class BootScene extends Phaser.Scene {
     constructor() {
-        super({ key: SCENES.BOOT });
+        super({ key: SCENE.BOOT });
     }
 
     preload() {
-        this._createLoadingBar();
+        this._buatLoadingBar();
+
+        this.load.spritesheet("player", "assets/sprites/Player.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+        this.load.spritesheet("player_aksi", "assets/sprites/Player_Actions.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+
+        this.load.spritesheet("musuh_rangka", "assets/sprites/Skeleton.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+        this.load.spritesheet("musuh_slime", "assets/sprites/Slime_Green.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+
+        this.load.spritesheet("rumah_warga", "assets/sprites/House.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+        this.load.spritesheet("rumah_besar", "assets/sprites/House_1_Wood_Base_Blue.png", {
+            frameWidth: 16,
+            frameHeight: 16,
+        });
+
+        //masih ada untuk dekorasi dan rumput
     }
 
     create() {
-        this._generatePlaceholder();
-        this.scane.start(SCENES.MAIN_MENU);
+        this._buatplaceholder();
+        this.scene.start(SCENE.MAIN_MENU);
     }
 
-    _createLoadingBar() {
+    _buatLoadingBar() {
         const { width, height } = this.scale;
         const cx = width / 2;
         const cy = height / 2;
 
-        this.add.text (
-            cx, cy - 60, "HARVEST SIEGE", {
-                fontFamily: "monospace",
-                fontSize: "28px",
-                color: "#e8d5a3",
-                letterSpacing: 6,
-            }
-        ).setOrigin(0.5);
+        this.add.text(cx, cy - 60,  "HARVEST SIEGE", {
+            fontFamily: "monospace",
+            fontSize: "28px",
+            color: "#e8d6a3",
+            letterSpacing: 6,
+        }).setOrigin(0,5);
 
-        const barBg = this.add.graphics();
-        barBg.fillStyle(0x333333, 1);
-        barBg.fillRoundedRect(cx - 160, cy - 10, 320, 20, 4);
+        const barLatar = this.add.graphics();
+        barLatar.fillStyle(0x333333, 1);
+        barLatar.fillRoundedRect(cx - 160, cy - 10, 320, 20, 4);
 
-        const barFill = this.add.graphics();
+        const barIsi = this.add.graphics();
 
-        const loadText = this.add.text(
-            cx, cy + 24, "Loading...", {
-                fontFamily: "monospace",
-                fontSize: "12px",
-                color: "#7a9e7e",
-            }
-        ).setOrigin(0.5);
+        const textLoad = this.add.text(cx, cy + 24, "Memuat...", {
+            fontFamily: "monospace",
+            fontSize: "12px",
+            color: "#7a9e7e",
+        }).setOrigin(0.5);
 
-        this.load.on("progress", (value) => {
-            barFill.clear();
-            barFill.fillStyle(0x7a9e7e, 1);
-            barFill.fillRoundedRect(cx - 158, cy - 8, 316 * value, 16, 3);
-            loadText.setText(`Loading... ${Math.floor(value * 100)}%`);
+        this.load.on("progress", (nilai) => {
+            barIsi.clear();
+            barIsi.fillStyle(0x7a9e7e, 1);
+            barIsi.fillRoundedRect(cx - 158, cy - 8, 316 * nilai, 16, 3);
+            textLoad.setText(`Memuat... ${Math.floor(nilai * 100)}%`);
         });
 
         this.load.on("complete", () => {
-            loadText.setText("Done!");
+            textLoad.setText("Siap!");
         });
     }
 
-    _generatePlaceholder() {
-        this._makeTexture("player", 24, 24, (g) => {
-            g.fillStyle(0x42a5f5, 1);
-            g.fillReact(0, 0, 24, 24);
-            g.fillStyle(0xffffff, 1);
-
-            g.fillTriangle(12, 2, 4, 18, 20, 18);
-        });
-
-        
-    }
+    
 }
